9e96ceade86ce3fb74789df86ccb0d716c762e49
MargoPae <91825234+MargoPae@users.noreply.github.com>
2022-05-21 14:46:09 +0300

    Added date

--- public/script.js
+++ public/script.js
@@ -27,3 +27,23 @@ getWeather("voru");
 getWeather("paide");
 getWeather("kardla");
 getWeather("rakvere");
+
+const days = ["Sunday","Monday","Tuesday","Wednesday","Thursday","Friday","Saturday"];
+const months = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"];
+let currentDate = new Date()
+let day = currentDate.getDate()
+
+let month = months[currentDate.getMonth()];
+let year = currentDate.getFullYear()
+
+
+
+//let hour = new Hour();
+//hour.getHours();
+
+const d = new Date();
+let dayOfWeek = days[d.getDay()];
+document.getElementById("date").innerText=` ${dayOfWeek} ${day} ${month} ${year}`;
+
+
+
