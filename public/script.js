161d3d0cbc4aa8e645d067b0f3ea0bce38e58a24
MargoPae <91825234+MargoPae@users.noreply.github.com>
2022-05-21 15:12:10 +0300

    Update script.js

--- public/script.js
+++ public/script.js
@@ -30,16 +30,12 @@ getWeather("rakvere");
 
 const days = ["Sunday","Monday","Tuesday","Wednesday","Thursday","Friday","Saturday"];
 const months = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"];
-let currentDate = new Date()
-let day = currentDate.getDate()
+let currentDate = new Date();
+let day = currentDate.getDate();
 
 let month = months[currentDate.getMonth()];
-let year = currentDate.getFullYear()
+let year = currentDate.getFullYear();
 
-
-
-//let hour = new Hour();
-//hour.getHours();
 
 const d = new Date();
 let dayOfWeek = days[d.getDay()];
